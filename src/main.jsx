10fae23f9218a44224014e8a1409ef189e5ba095
4dogfarm <4dogfarmllc266@gmail.com>
2026-07-13 17:17:51 -0700

    Initialize main.jsx with React setup

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+
+Mieke <4dogfarmllc266@gmail.com>
+5:16 PM (0 minutes ago)
+to me
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CompanionGarden from './companion-garden'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CompanionGarden />
+  </React.StrictMode>
+)
