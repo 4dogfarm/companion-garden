86588208fee5af2c2e4c3d73f85572f56d826a0d
4dogfarm <4dogfarmllc266@gmail.com>
2026-07-20 17:25:51 -0700

    Remove email comments from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,3 @@
-
-Mieke <4dogfarmllc266@gmail.com>
-5:16 PM (0 minutes ago)
-to me
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CompanionGarden from './companion-garden'
